e7cc667841d2015d5671c192a4a3704f3403dafa
RevinV <vlad.revin.95@mail.ru>
2020-07-29 20:42:43 +0300

    addProject

---
+++ FourArtsOneWord/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FourArtsOneWord
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MainWindow startWindow;
+        public MainWindow()
+        {
+            startWindow = this;
+            InitializeComponent();
+            
+
+            
+
+        }
+
+        private void Lvl1_Click(object sender, RoutedEventArgs e)
+        {
+            LevelOne lvl1 = new LevelOne();
+            this.Close();
+            LevelOne.lvl1.Show();
+        }
+    }
+}
---
+++ FourArtsOneWord/WordButton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace FourArtsOneWord
+{
+    class WordButton
+    {
+        char name;
+
+        //public void CreateButton(char name)
+        //{
+        //    Button button = new Button();
+        //    button.Content = name;
+        //    button.Height = 45;
+        //    button.Click += (object sender, RoutedEventArgs e) => ;
+        //    word.Children.Add(button);
+        //}
+    }
+}
